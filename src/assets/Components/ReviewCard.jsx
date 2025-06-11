921f24f4710613e78b21e7fecd69dadb32fbac27
Ghanendra Rana <ghanendrarana.dev@gmail.com>
2025-06-11 12:26:57 +0545

    Few css changes

--- src/assets/Components/ReviewCard.jsx
+++ src/assets/Components/ReviewCard.jsx
@@ -7,40 +7,30 @@ import PlatformLogo from "./PlatformLogo";
 import Stars from "./Stars";
 import styles from "./reviewcard.module.css";
 
-const ReviewCard = ({
-  logo,
-  qr,
-  hotelName,
-  tagline,
-  platformLogo,
-}) => {
+const ReviewCard = ({ logo, qr, hotelName, tagline, platformLogo }) => {
   return (
-    <div>
-      <div className={styles.reviewCard}>
-        <div className="logo-container">
-          <HotelLogo logo={logo} />
-        </div>
-
-        <div>
-          <HotelName hotelName={hotelName} />
-        </div>
-
-        <div>
-          <Tagline tagline={tagline} />
-        </div>
-
-        <div className="qr-container">
-          <QrCode qr={qr} />
-        </div>
-
-        {/* <div className="heart">❤️</div> */}
-
-        <div className="platform">
-          <PlatformLogo platformLogo={platformLogo} />
-        </div>
-
-       <Stars />
+    <div className={styles.reviewCard}>
+      <div className="logo-container">
+        <HotelLogo logo={logo} />
       </div>
+
+      <div>
+        <HotelName hotelName={hotelName} />
+      </div>
+
+      <div>
+        <Tagline tagline={tagline} />
+      </div>
+
+      <div className="qr-container">
+        <QrCode qr={qr} />
+      </div>
+
+      <div className="platform">
+        <PlatformLogo platformLogo={platformLogo} />
+      </div>
+
+      <Stars />
     </div>
   );
 };
